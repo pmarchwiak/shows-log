037e6cd402688081a1620dd7c21006c4311b6d74
Patrick Marchwiak <pmarchwiak@gmail.com>
2023-06-11 09:53:17 -0700

    Cleanup gsheet export

--- scripts/gsheet-to-tsv.py
+++ scripts/gsheet-to-tsv.py
@@ -16,23 +16,6 @@ def export_to_tsv(sheet_url, output_file):
 
     print(f"Wrote tsv to {output_file} successfully!")
 
-# def tsv_to_json(tsv_path):
-#     # Open the TSV file for reading
-#     with open(tsv_path, 'r', newline='') as tsvfile:
-#         reader = csv.DictReader(tsvfile, delimiter='\t')
-
-#         for row in reader:
-#             if 
-
-#         rows = [row for row in reader]
-
-#     # Write the data as JSON to a file
-#     with open(json_file, 'w') as jsonfile:
-#         json.dump(rows, jsonfile, indent=4)
-
-#     print(f"Conversion complete. JSON file saved as {json_file}.")
-
-
 sheet_url = 'https://docs.google.com/spreadsheets/d/1XmjKYOYZdxDeAPlJTmINBti4fLIb6kq0iXgdf72kQLc/edit?usp=sharing'
 
 tsv_path = "data/showslog-data-" + datetime.now().strftime("%Y-%m-%d_%H%M%S") + ".tsv"
